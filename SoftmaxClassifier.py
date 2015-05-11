Fixing softmax classifier

SoftmaxClassifier.py
@@ -22,23 +22,26 @@ class SoftmaxClassifier(object):
 
         #generate random weights for W
         if W1 == None:
-            val_range1 = [-sqrt(6.0/(n_inputs+n_outputs+1)), sqrt(6.0/(n_inputs+n_outputs+1))]
-            W1 = val_range1[0] + np.random.random_sample((n_outputs, n_inputs))*2.0*val_range1[1]
+            val_range1 = [0, 2*sqrt(6.0/(n_inputs+n_outputs+1))]
+            W1 = np.random.random_sample((n_outputs, n_inputs))*0.002
             self.W1 = W1
 
 
         #by introducing *0.05 to b1 initialization got an error dropoff from 360 -> 280
         if b1 == None:
-            b1 = -0.01 + np.random.random_sample((n_outputs,)) * 0.02
+            b1 = np.random.random_sample((n_outputs,)) * 0.02
             self.b1 = b1
 
 
-    def forward_pass_for_one_case(self, x, W1, b1):
+    def forward_pass(self, x, W1, b1):
 
-        z2 = np.dot(W1, x) + b1
-        a2 = self.sigmoid(z2)
+        x_bias = np.concatenate((x,np.array([1])),axis=0)
+        theta_mat = self.getThetaMatrix(W1,b1)
 
-        return a2
+        top = np.exp(np.dot(theta_mat,x_bias))
+        bottom = np.sum(np.exp(np.dot(theta_mat,x_bias)))
+
+        return top/bottom
 
     def packTheta(self, W1, b1):
         theta = np.concatenate((np.reshape(W1, (self.n_outputs*self.n_inputs,)), b1))
@@ -52,6 +55,9 @@ class SoftmaxClassifier(object):
 
         return W1, b1
 
+    def getThetaMatrix(self, W1, b1):
+        theta_mat =  np.append(W1,b1[:,None],axis=1)
+        return theta_mat
 
     # cost calculate the cost you get given all the inputs feed forward through the network
     # at the moment I am using the squared error between the reconstructed and the input
@@ -66,9 +72,18 @@ class SoftmaxClassifier(object):
 
         for idx in range(size_data):
             x = data[:, idx]
-            a2 = self.forward_pass_for_one_case(x, W1, b1)
+            x_bias = np.concatenate((x,np.array([1])),axis=0)
 
-            err = -np.log(a2[labels[idx]]/np.sum(a2))
+            y = labels[idx]
+
+            theta_mat = self.getThetaMatrix(W1,b1)
+            theta_k = theta_mat[y,:]
+            tmp = np.dot(theta_mat,x_bias)
+            log_top = np.exp(tmp)
+            log_bottom = np.sum(np.exp(np.dot(theta_mat,x_bias)))
+            tmp2 = np.log(log_top/log_bottom)
+            err = -np.sum(tmp2)
+
             tot_err += err
 
         tot_err = tot_err/size_data
@@ -78,12 +93,11 @@ class SoftmaxClassifier(object):
     # Cost prime is the gradient of the cost function.
     # In other words this is dC/dW in the delta rule (i.e. W = W - alpha*dC/dW)
     # make sure to send 5,4,9.. type of values for the 'labels', not the vectorized form
-    def cost_prime(self,theta,data,labels):
+    def cost_prime(self, theta, data, labels):
 
         W1, b1 = self.unpackTheta(theta)
 
         d_theta = np.zeros((self.n_outputs, self.n_inputs+1), dtype=np.float32)
-
 
         size_data = data.shape[1]
         for idx in range(size_data):
@@ -93,15 +107,21 @@ class SoftmaxClassifier(object):
             y = labels[idx]
             y_vec = [0.0] * self.n_outputs
             y_vec[y] = 1.0
-            a2 = self.forward_pass_for_one_case(x, W1, b1)
 
-            delta = y_vec - np.log(a2[y]/np.sum(a2))
-            tmp_arr = np.dot(delta[:, None], np.transpose(x_bias[:, None]))
+            theta_mat = self.getThetaMatrix(W1, b1)
+            theta_k = theta_mat[y, :]
+            tmp = np.dot(theta_k.T,x_bias)
+            log_top = np.exp(tmp)
+
+            tmp2 = np.dot(theta_mat,x_bias)
+            log_bottom = np.sum(np.exp(tmp2))
+
+            delta = y_vec - np.log(log_top/log_bottom)
+
+            tmp_arr = -np.dot(delta[:, None], np.transpose(x_bias[:, None]))
             d_theta = d_theta + tmp_arr
 
-
         d_theta = ((1.0/size_data) * d_theta)
-        #d_b1 = (1.0/size_data) * d_b1
 
         return np.reshape(d_theta,(self.n_outputs*(self.n_inputs+1),))
 

StackedAutoencoder.py
@@ -56,7 +56,7 @@ class StackedAutoencoder(object):
         data_size = self.X.shape[1]
 
         sa1 = SparseAutoencoder(n_inputs=in_dim,n_hidden=h1_dim,X=self.X)
-        sa1.back_prop(iter=3)
+        sa1.back_prop(iter=1)
         W1_1,b1_1,W1_2,b1_2 = sa1.get_params()
         self.W1_1 = W1_1
 
@@ -70,7 +70,7 @@ class StackedAutoencoder(object):
 
         print ("Inputs for 2nd AE created. Size (%i, %i)" %(X2.shape[0],X2.shape[1]))
         sa2 = SparseAutoencoder(n_inputs=h1_dim,n_hidden=h2_dim,X=X2)
-        sa2.back_prop(iter=3)
+        sa2.back_prop(iter=1)
         W2_1,b2_1,W2_2,b2_2 = sa2.get_params()
         self.W2_1 = W2_1
 
@@ -85,7 +85,7 @@ class StackedAutoencoder(object):
 
         print ("Inputs for 2nd AE created. Size (%i, %i)" %(X3.shape[0],X3.shape[1]))
         sa3 = SparseAutoencoder(n_inputs=h2_dim, n_hidden=h3_dim, X=X3)
-        sa3.back_prop(iter=3)
+        sa3.back_prop(iter=1)
         W3_1,b3_1,W3_2,b3_2 = sa3.get_params()
         self.W3_1 = W3_1
 
@@ -101,13 +101,13 @@ class StackedAutoencoder(object):
         print ("Inputs for 3rd AE created. Size (%i, %i)" %(X4.shape[0],X4.shape[1]))
 
         softmax = SoftmaxClassifier(n_inputs=h3_dim, n_outputs=self.o_size, X=X4, Y=self.Y_VEC)
-        softmax.back_prop(iter=3)
+        softmax.back_prop(iter=50)
         W4_1,b4_1 = softmax.get_params()
 
         output = np.zeros([self.o_size,data_size],dtype=np.float32)
         idx = 0
         for x in X4.T:
-            a2 = softmax.forward_pass_for_one_case(x,W4_1,b4_1)
+            a2 = softmax.forward_pass(x,W4_1,b4_1)
             output[:,idx] = a2
             idx += 1
 