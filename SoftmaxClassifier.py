Fixed the issue with softmax classifier (correct imple: back_prop_man) The issue has been in the gradient function.

SoftmaxClassifier.py
@@ -4,6 +4,7 @@ from math import sqrt
 from math import isnan,isinf
 import numpy as np
 from scipy import optimize
+import numpy.linalg as LA
 class SoftmaxClassifier(object):
 
     def sigmoid(self, x):
@@ -39,7 +40,7 @@ class SoftmaxClassifier(object):
         theta_mat = self.getThetaMatrix(W1,b1)
 
         top = np.exp(np.dot(theta_mat,x_bias))
-        bottom = np.sum(np.exp(np.dot(theta_mat,x_bias)))
+        bottom = np.sum(top)
 
         return top/bottom
 
@@ -58,6 +59,11 @@ class SoftmaxClassifier(object):
     def getThetaMatrix(self, W1, b1):
         theta_mat =  np.append(W1,b1[:,None],axis=1)
         return theta_mat
+
+    def getWeightsAndBias(self, theta_mat):
+        W1 = theta_mat[:,0:theta_mat.shape[1]-1]
+        b1 = theta_mat[:,theta_mat.shape[1]-1]
+        return W1,b1
 
     # cost calculate the cost you get given all the inputs feed forward through the network
     # at the moment I am using the squared error between the reconstructed and the input
@@ -115,30 +121,25 @@ class SoftmaxClassifier(object):
             y_vec[y] = 1.0
 
             theta_mat = self.getThetaMatrix(W1, b1)
-            theta_k = theta_mat[y, :]
-            tmp = np.dot(theta_k.T,x_bias)
-            top = np.exp(tmp)
 
-            tmp2 = np.dot(theta_mat,x_bias)
-            tmp3 = np.exp(tmp2)
-            bottom = np.sum(tmp3)
+            W1,b1 = self.getWeightsAndBias(theta_mat)
 
-            if bottom == 0. or isnan(bottom) or isinf(bottom) :
-                print "test"
+            topDevBottom = self.forward_pass(x,W1,b1)[y]
 
-            delta = y_vec - (top/bottom)
+            delta = np.asarray(y_vec,dtype=np.float32)
+            delta[y,None] = delta[y,None]-topDevBottom
 
-            tmp_arr = -np.dot(delta[:, None], np.transpose(x_bias[:, None]))
+            tmp_arr = np.dot(delta[:, None], np.transpose(x_bias[:, None]))
             d_theta = d_theta + tmp_arr
 
-        d_theta = ((1.0/size_data) * d_theta) + lam*theta_mat
+        d_theta = -((1.0/size_data) * d_theta) + lam*theta_mat
 
         return np.reshape(d_theta,(self.n_outputs*(self.n_inputs+1),))
 
     def back_prop(self, iter=1000):
         init_val = self.packTheta(self.W1, self.b1)
         #err = optimize.check_grad(self.cost, self.cost_prime, init_val, self.X,self.Y)
-        res = optimize.minimize(fun=self.cost, x0=init_val, args=(self.X,self.Y,0.0001), jac=self.cost_prime, method='BFGS', options={'maxiter':iter,'disp':True})
+        res = optimize.minimize(fun=self.cost, x0=init_val, args=(self.X,self.Y,0.0001), jac=self.cost_prime, method='L-BFGS-B', options={'maxiter':iter,'disp':True})
         self.W1, self.b1 = self.unpackTheta(res.x)
 
         #print ("Error (check_grad): %f" %err)
@@ -148,9 +149,10 @@ class SoftmaxClassifier(object):
         theta_mat = self.getThetaMatrix(self.W1, self.b1)
 
         for i in range(iter):
-            print ("Iteration: %i"%i)
+
             d_theta = np.zeros((self.n_outputs, self.n_inputs+1), dtype=np.float32)
             size_data = self.X.shape[1]
+            tot_err = 0.0
 
             for idx in range(size_data):
                 x = self.X[:,idx]
@@ -160,23 +162,21 @@ class SoftmaxClassifier(object):
                 y_vec = [0.0] * self.n_outputs
                 y_vec[y] = 1.0
 
+                W1,b1 = self.getWeightsAndBias(theta_mat)
+                topDevBottom = self.forward_pass(x,W1,b1)
 
-                theta_k = theta_mat[y, :]
-                tmp = np.dot(theta_k.T,x_bias)
-                top = np.exp(tmp)
+                delta = np.asarray(y_vec,dtype=np.float32)
+                delta = delta-topDevBottom
+                err = LA.norm(delta)
+                tot_err = tot_err + err
 
-                tmp2 = np.dot(theta_mat,x_bias)
-                tmp3 = np.exp(tmp2)
-                bottom = np.sum(tmp3)
-
-                delta = y_vec - (top/bottom)
-
-                tmp_arr = -np.dot(delta[:, None], np.transpose(x_bias[:, None]))
+                tmp_arr = np.dot(delta[:, None], np.transpose(x_bias[:, None]))
                 d_theta = d_theta + tmp_arr
 
-            d_theta = ((1.0/size_data) * d_theta) + lam*theta_mat
-
-            theta_mat = theta_mat - alpha*d_theta
+            d_theta = -((1.0/size_data) * d_theta) + (lam*theta_mat)
+            tot_err = (1.0/size_data)*tot_err
+            print ("Iteration: %i, Error: %f"%(i,tot_err))
+            theta_mat = theta_mat - (alpha*d_theta)
 
 
         self.W1 = theta_mat[:,0:theta_mat.shape[1]-1]

SparseAutoencoder.py
@@ -46,8 +46,8 @@ class SparseAutoencoder(object):
         self.min_batch_X = np.zeros((n_inputs, m_batch_size), dtype=np.float32)
 
         self.lam = 0.0001
-        self.beta = 0.5
-        self.rho = 0.25
+        self.beta = 0.1
+        self.rho = 0.3
 
         self.rho_nh = np.zeros((self.n_hidden,),dtype=np.float32)
 

StackedAutoencoder.py
@@ -49,14 +49,14 @@ class StackedAutoencoder(object):
     def train_model(self):
 
         in_dim = self.img_w*self.img_h
-        h1_dim = 15*15
-        h2_dim = 10*10
-        h3_dim = 8*8
+        h1_dim = 15**2
+        h2_dim = 10**2
+        h3_dim = 8**2
 
         data_size = self.X.shape[1]
 
         sa1 = SparseAutoencoder(n_inputs=in_dim,n_hidden=h1_dim,X=self.X)
-        sa1.back_prop(iter=4)
+        sa1.back_prop(iter=25)
         W1_1,b1_1,W1_2,b1_2 = sa1.get_params()
         self.W1_1 = W1_1
 
@@ -70,7 +70,7 @@ class StackedAutoencoder(object):
 
         print ("Inputs for 2nd AE created. Size (%i, %i)" %(X2.shape[0],X2.shape[1]))
         sa2 = SparseAutoencoder(n_inputs=h1_dim,n_hidden=h2_dim,X=X2)
-        sa2.back_prop(iter=15)
+        sa2.back_prop(iter=80)
         W2_1,b2_1,W2_2,b2_2 = sa2.get_params()
         self.W2_1 = W2_1
 
@@ -85,7 +85,7 @@ class StackedAutoencoder(object):
 
         print ("Inputs for 2nd AE created. Size (%i, %i)" %(X3.shape[0],X3.shape[1]))
         sa3 = SparseAutoencoder(n_inputs=h2_dim, n_hidden=h3_dim, X=X3)
-        sa3.back_prop(iter=15)
+        sa3.back_prop(iter=80)
         W3_1,b3_1,W3_2,b3_2 = sa3.get_params()
         self.W3_1 = W3_1
 
@@ -101,7 +101,7 @@ class StackedAutoencoder(object):
         print ("Inputs for 3rd AE created. Size (%i, %i)" %(X4.shape[0],X4.shape[1]))
 
         softmax = SoftmaxClassifier(n_inputs=h3_dim, n_outputs=self.o_size, X=X4, Y=self.Y_VEC)
-        softmax.back_prop_man(iter=50)
+        softmax.back_prop_man(iter=250)
         W4_1,b4_1 = softmax.get_params()
 
         output = np.zeros([self.o_size,data_size],dtype=np.float32)
@@ -112,6 +112,20 @@ class StackedAutoencoder(object):
             idx += 1
 
         print ("Finished Training")
+
+        outDigits = np.empty((1,self.d_size),dtype=np.int8)
+        totCorrect = 0
+        for i in range(self.d_size-1):
+            maxCol = np.max(output[:,i])
+            if maxCol>0.4:
+                maxIdx = np.argmax(output[:,i])
+                #outDigits[1,i]=maxIdx
+                if self.Y_VEC[i]==maxIdx:
+                    totCorrect = totCorrect+1
+            #else:
+                #outDigits[1,i]=-1
+
+        print ("Accuracy: %f" %(1.0*totCorrect/self.d_size))
 
     def mkdir_if_not_exist(self, name):
         if not os.path.exists(name):